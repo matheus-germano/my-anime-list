3f2130f0f2f929d34004f63587119e32082ee927
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-08-10 21:17:47 -0300

    :lipstick: feat: Anime Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,13 @@
-import { useState } from "react"
 import { AnimeCard } from "./components/AnimeCard"
-import { Anime } from "./types/anime"
+
+import { animeMock } from "./mocks/anime"
 
 import "./index.css"
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
-  function handleButtonClick() {
-    setCount(prevState => prevState + 1)
-  }
-
-  const animeMock: Anime = {
-    mal_id: 1,
-    url: "https://myanimelist.net/anime/1/Cowboy_Bebop",
-    title: "Anime mock",
-    synopsis: "Anime mock synopsis",
-    images: {
-      image_url: "https://cdn.myanimelist.net/images/anime/4/19644.jpg",
-      small_image_url: "https://cdn.myanimelist.net/images/anime/4/19644t.jpg",
-      large_image_url: "https://cdn.myanimelist.net/images/anime/4/19644l.jpg"
-    },
-    episodes: 26,
-    score: 8.75,
-    genres: [
-      {
-        mal_id: 1,
-        type: "anime",
-        name: "Action",
-        url: "https://myanimelist.net/anime/genre/1/Action",
-      }
-    ]
-  }
-
   return (
     <>
       <AnimeCard anime={animeMock} />
-      <h1>Hello</h1>
-      <p data-testid="state-value">{count}</p>
-      <button onClick={handleButtonClick}>increase</button>
     </>
   )
 }
--- src/components/AnimeCard/index.tsx
+++ src/components/AnimeCard/index.tsx
@@ -1,4 +1,7 @@
-import { Plus, WarningCircle } from "phosphor-react"
+import { Heart, Star, WarningCircle } from "phosphor-react"
+
+import { Button } from "../Button"
+
 import { Anime } from "../../types/anime"
 
 interface AnimeCardProps {
@@ -33,17 +36,22 @@ export function AnimeCard({ anime, isLoading = false }: AnimeCardProps) {
   return (
     <div 
       data-testid="anime-card"
-      className="w-64 h-[456px] bg-zinc-200 flex flex-col rounded-lg overflow-hidden relative"
+      className="w-64 bg-zinc-200 flex flex-col rounded-lg overflow-hidden relative"
     >
       <div className="h-2/4">
         <img 
           src={anime.images.image_url}
           alt={anime.title}
-          className="h-full w-full object-cover rounded-t-lg"
+          className="h-64 w-full object-cover rounded-t-lg"
         />
         <div className="w-full flex justify-between items-center absolute top-0 p-4">
-          <p>{anime.score}</p>
-          <Plus size={24} />
+          <div className="flex gap-1 items-center bg-zinc-900 bg-opacity-50 border border-solid border-zinc-600 rounded-lg p-2">
+            <p className="text-zinc-50">{anime.score}</p>
+            <Star className="text-yellow-300" size={20} weight="fill" />
+          </div>
+          <div className="flex gap-1 items-center bg-zinc-900 bg-opacity-50 border border-solid border-zinc-600 rounded-lg p-2">
+            <Heart className="text-zinc-50 hover:text-red-500 cursor-pointer" size={20} />
+          </div>
         </div>
       </div>
       <div className="h-full flex flex-col p-4 gap-4">
@@ -51,17 +59,17 @@ export function AnimeCard({ anime, isLoading = false }: AnimeCardProps) {
           {anime.genres.length > 0 ? anime.genres.map(gender => (
             <p
               key={gender.mal_id}
-              className="w-fit text-xs p-1 bg-zinc-100 rounded-md"
+              className="w-fit text-xs p-2 text-zinc-700 bg-zinc-100 rounded-md border border-solid border-white"
             >{gender.name}</p>
           )) : (<p>This anime does not has a gender</p>)}
         </div>
-        <div className="flex justify-between items-center">
-          <h3 className="font-black text-lg">{anime.title}</h3>
-          <p>{anime.episodes} episodes</p>
+        <div className="flex flex-col justify-between items-start">
+          <h3 className="font-black text-lg text-ellipsis overflow-hidden">{anime.title}</h3>
+          <p className="text-xs text-zinc-700">{String(anime.episodes).padStart(4, "0") + " episodes"}</p>
         </div>
-        <div className="h-full flex flex-col justify-between">
-          <p>{anime.synopsis}</p>
-          <p>teste</p>
+        <div className="h-full flex flex-col gap-3 justify-between">
+          <p className="text-zinc-700 text-ellipsis overflow-hidden line-clamp-3 text-justify">{anime.synopsis}</p>
+          <Button>Avaliar</Button>
         </div>
       </div>
     </div>
